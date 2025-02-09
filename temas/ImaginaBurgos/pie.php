e676da3f392430b8d938836245cabbf12a217f42
Silviatg <silviategonzalez@gmail.com>
2025-02-09 21:34:11 +0100

    Add files via upload

---
+++ temas/ImaginaBurgos/pie.php
@@ -0,0 +1,10 @@
+
+ <div  class="pie_demokratia"> 
+ 
+ <div class="pie_demokratia2"> 
+ 	<a href="http://www.demokratian.org" target="_blank"><img src="../temas/<?php echo "$tema_web"; ?>/imagenes/logo_pie.png" class="img-responsive"  alt="DEMOKRATIA | plataforma de votaciones"  /></a>
+    </div>
+        <div class="pie_demokratia1">  Aplicación realizada por Carlos Salgado<br>Adaptada para Imagina Burgos<br>Contacto: votaciones@imaginaburgos.es</div>
+     
+</div>
+
